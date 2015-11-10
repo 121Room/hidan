e354f0bf93cc5fdd03b14f5759d41757f9785e30
JIANG Di <jiang-di@outlook.com>
2015-11-10 19:07:18 +0800

    get data from single

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express')
-var router = express.Router()
+'use strict'
+
+let express = require('express')
+let router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' })
+})
+
+router.post('/report', (req, res, next) => {
+  console.log(req.body) // TODO save to db
+  res.send(req.body)
 })
 
 module.exports = router
