b9bbd0bc2ffc3881ab59a24d927234a6b5e65e53
JIANG Di <jiang-di@outlook.com>
2015-11-13 20:03:42 +0800

    clean up

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 let express = require('express')
 let path = require('path')
-let favicon = require('serve-favicon')
 let logger = require('morgan')
 let cookieParser = require('cookie-parser')
 let bodyParser = require('body-parser')
@@ -11,17 +10,11 @@ let app = express()
 let cors = require('cors')
 app.use(cors())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'hbs')
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.post('/report', (req, res, next) => {
   console.log(req.body) // TODO save to db
