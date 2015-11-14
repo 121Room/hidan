847b43313d9172165b03638f811fcb65467fbd74
JIANG Di <jiang-di@outlook.com>
2015-11-14 16:50:01 +0800

    rewrite error handler

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 if (app.get('env') === 'development') {
   app.use((err, req, res, next) => {
     res.status(err.status || 500)
-    res.render('error', {
+    res.send({
       message: err.message,
       error: err
     })
@@ -46,9 +46,8 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
   res.status(err.status || 500)
-  res.render('error', {
-    message: err.message,
-    error: {}
+  res.send({
+    message: err.message
   })
 })
 
